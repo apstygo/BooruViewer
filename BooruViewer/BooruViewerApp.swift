9e024791150f3dde38d834a5f94c7b20b41ebe51
Artem Pstygo <apstygo.dev@gmail.com>
2022-10-11 11:17:44 +0200

    Initial Commit

---
+++ BooruViewer/BooruViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooruViewerApp.swift
+//  BooruViewer
+//
+//  Created by Artem Pstygo on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct BooruViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
