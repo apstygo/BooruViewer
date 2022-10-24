10fbfaa3864be3d0ab7197c9310d2611237ff369
Artem Pstygo <apstygo.dev@gmail.com>
2022-10-24 21:33:24 +0200

    extend AuthInfo with validity checks

--- BooruViewer/Sources/Model/Authorization/AuthInfo.swift
+++ BooruViewer/Sources/Model/Authorization/AuthInfo.swift
@@ -6,3 +6,51 @@ struct AuthInfo: Codable {
     let accessToken: String
     let refreshToken: String
 }
+
+extension AuthInfo {
+    var isValid: Bool {
+        guard let expirationDate else {
+            return false
+        }
+
+        return expirationDate > .now
+    }
+
+    var expirationDate: Date? {
+        let components = accessToken.split(separator: ".")
+
+        guard
+            components.count == 3,
+            let data: Data = .fromBase64(String(components[1])),
+            let jsonObject = (try? JSONSerialization.jsonObject(with: data)) as? [String: Any],
+            let exp = jsonObject["exp"] as? TimeInterval
+        else {
+            return nil
+        }
+
+        return Date(timeIntervalSince1970: exp)
+    }
+}
+
+// MARK: - Helpers
+
+extension Data {
+
+    fileprivate static func fromBase64(_ encoded: String) -> Data? {
+        // Prefixes padding-character(s) (if needed).
+        var encoded = encoded
+        let remainder = encoded.count % 4
+
+        if remainder > 0 {
+            encoded = encoded.padding(
+                toLength: encoded.count + 4 - remainder,
+                withPad: "=",
+                startingAt: 0
+            )
+        }
+
+        // Finally, decode.
+        return Data(base64Encoded: encoded)
+    }
+
+}
